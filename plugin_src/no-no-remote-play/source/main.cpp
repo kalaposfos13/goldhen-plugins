a57ca801452cbab33932f229e1515f75564ac111
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2026-09-05 11:28:20 +0200

    it be workin'

--- plugin_src/no-no-remote-play/source/main.cpp
+++ plugin_src/no-no-remote-play/source/main.cpp
@@ -14,18 +14,40 @@
 #include <unordered_map>
 #include <vector>
 
+#include <orbis/Remoteplay.h>
+#include <orbis/Sysmodule.h>
 #include <orbis/libkernel.h>
 
 extern "C" {
 
 attr_public const char* g_pluginName = "no no-remote-play";
 attr_public const char* g_pluginDesc = "";
-attr_public const char* g_pluginAuth = "kalaposfos, metr1k";
+attr_public const char* g_pluginAuth = "kalaposfos";
 attr_public u32 g_pluginVersion = 0x00000100; // 1.00
 char titleid[16];
 
 // hook_inits and functions
+HOOK_INIT(sceRemoteplayProhibit);
+int sceRemoteplayProhibit_hook() {
+    // LOG_INFO("(STUBBED) called");
+    return 0;
+}
 
+HOOK_INIT(sceSysmoduleLoadModule);
+int sceSysmoduleLoadModule_hook(OrbisSysModule const id) {
+    LOG_INFO("called, id: {:#x}", (u16)id);
+    s32 ret = HOOK_CONTINUE(sceSysmoduleLoadModule, s32 (*)(OrbisSysModule const), id);
+    switch (id) {
+    case ORBIS_SYSMODULE_REMOTE_PLAY:
+        LOG_INFO("Hooking remoteplay");
+        HOOK(sceRemoteplayProhibit);
+        break;
+
+    default:
+        break;
+    }
+    return ret;
+}
 
 s32 attr_public plugin_load(s32 argc, const char* argv[]) {
     final_printf("[GoldHEN] <%s\\Ver.0x%08x> %s\n", g_pluginName, g_pluginVersion, __func__);
@@ -37,12 +59,15 @@ s32 attr_public plugin_load(s32 argc, const char* argv[]) {
         print_proc_info();
     }
     // hooks
+    HOOK32(sceSysmoduleLoadModule);
     return 0;
 }
 
 s32 attr_public plugin_unload(s32 argc, const char* argv[]) {
     final_printf("[GoldHEN] <%s\\Ver.0x%08x> %s\n", g_pluginName, g_pluginVersion, __func__);
     // unhooks
+    UNHOOK(sceSysmoduleLoadModule);
+    UNHOOK(sceRemoteplayProhibit);
     return 0;
 }
 
